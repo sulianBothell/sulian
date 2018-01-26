cc690eb2425667cd7f95166415eba398d49a6039
sulian <sulian@sulians-Air.fios-router.home>
2018-01-26 00:09:13 -0800

    echo map

--- population/select.php
+++ population/select.php
@@ -1,17 +1,18 @@
 <?php
+header('Content-type: image/svg+xml');
 try{
   require_once('/var/www/html/conn/conn.php');
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connection successfully!</br>";
+  //echo "Connection successfully!</br>";
   }
 catch(PDOException $e){
-  echo "Connection failed: ". $e->getMessage();
+  //echo "Connection failed: ". $e->getMessage();
 }
 $query  = $db->prepare("SELECT * FROM `country_map` WHERE `country` = :country");
 $b = 'China';
 $query->bindParam(':country', $b);
 $query->execute();
 while($row = $query->fetch(PDO::FETCH_ASSOC)) {
-  print_r($row);
+  echo $row['map'];
 }
  ?>
